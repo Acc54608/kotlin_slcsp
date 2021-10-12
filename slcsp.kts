95007b06f4ee63789bca5b1c5c698c1abf174f34
Alex Casper <84734863+Acc54608@users.noreply.github.com>
2021-10-12 10:39:10 -0400

    Update slcsp.kts

--- slcsp.kts
+++ slcsp.kts
@@ -23,7 +23,7 @@ reader.open(slc){
     }
 }
 
-// 
+// This foreach reader is for finding each zipcode needed for the solution in zipcodes.csv and giving each zipcode key in zipMap the rateArea as the value
 reader.open(zip){
     readAllAsSequence().forEach { row: List<String> ->
         val rateArea = Pair(row[1], row[4]).toString()
@@ -31,7 +31,8 @@ reader.open(zip){
             if (zipmap[row[0]].isNullOrEmpty()) {
                 zipmap[row[0]] = rateArea
                 ratePlans[rateArea] = mutableListOf()
-            }
+            } 
+            // If the program finds another rateArea for a certain zipcode then that zipocode is ambiguous and the slcsp can't be found so its value will be empty
             else if (zipmap[row[0]] != rateArea) {
                 zipmap[row[0]] = ""
                 ratePlans.remove(rateArea)
@@ -43,6 +44,7 @@ reader.open(zip){
 reader.open(plans) {
     readAllAsSequence().forEach { row: List<String> ->
         val rateArea = Pair(row[1], row[4]).toString()
+        // This if will simply find each silver plan for a matching rate area ans add it to a list so we can compare them later
         if (ratePlans.containsKey(rateArea)) {
             if (row[2] == "Silver")
                 ratePlans[rateArea]?.add(row[3].toDouble())
@@ -52,15 +54,19 @@ reader.open(plans) {
 
 ratePlans.forEach { entry ->
     entry.value.sort()
+    // Sort the entry's value to get them in order to easily find the 2nd lowest then I filter entries because 2 zipcodes can have the same rate area
     val zipMatches = zipmap.entries.filter { it.value == entry.key }
     zipMatches.forEach { zip ->
+        // Another loop for the filtered entries incase of doubles then assign each zipcode key in zipmap the 2nd lowest cost plan 
         if (!entry.value.isNullOrEmpty() && entry.value.size > 2)
             zipmap[zip.key] = String.format("%.2f", entry.value[1])
-        else
+        else 
+        // In case of the zip having more rate areas from the 2nd loop the answer is empty or if it has 1 or 0 silver plans it doesn't have a 2nd lowest.
             zipmap[zip.key] = ""
     }
 }
 
+// This just writes the answer into a new file then then I open that file and read it to sdtout
 writer.open("slcspChanged.csv") {
     var row = listOf("zipcode", "rate")
     writeRow(row)
